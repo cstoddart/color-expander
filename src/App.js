187112932b3039dc1993f8e89120ad2ecee7982e
Chris Stoddart <cstoddart@rocketmail.com>
2020-05-26 01:36:37 -0500

    add secondary and tertiary colors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import { expandColor, formatHex } from './utilities';
 import { GlobalStyles } from './globalStyles';
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const Input = styled.input``;
 
@@ -10,60 +16,190 @@ const Results = styled.div`
   display: flex;
   align-items: center;
   box-shadow : 0 0 1px 1px rgba(0,0,0,0.1);
+  width: 100%;
+`;
+
+const ResultTooltip = styled.div`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  margin-top: 10px;
+  padding: 5px;
+  box-shadow : 0 0 1px 1px rgba(0,0,0,0.1);
+  z-index: 1;
+  background-color: white;
+  opacity: 0;
 `;
   
-  const Result = styled.div`
+const StyledResult = styled.div.attrs(({ color }) => ({
+  style: { background: `#${color}` },
+}))`
   width: 100px;
   height: 100px;
-  background-color: #${({ color }) => color};
   position: relative;
-  margin: ${({ active }) => active && '10px'};
 
-  &:hover:after {
-    content: '#${({ color }) => formatHex(color)}';
-    position: absolute;
-    top: 100%;
-    left: 0;
-    margin-top: 10px;
-    padding: 5px;
-    box-shadow : 0 0 1px 1px rgba(0,0,0,0.1);
+  &:hover ${ResultTooltip} {
+    opacity: 1;
   }
 `;
 
-export const App = () => {
-  const [inputValue, setInputValue] = useState('');
-  const [colorHex, setColorHex] = useState(null);
-  const [colorSet, setColorSet] = useState([]);
+const Result = ({ color }) => (
+  <StyledResult color={color}>
+    <ResultTooltip>#{formatHex(color)}</ResultTooltip>
+  </StyledResult>
+);
 
-  const handleChange = ({ target: { value } }) => setInputValue(value);
+export const App = () => {
+  const [inputValue, setInputValue] = useState('123456');
+  const [primaryColorSetA, setPrimaryColorSetA] = useState([]);
+  const [primaryColorSetB, setPrimaryColorSetB] = useState([]);
+  const [primaryColorSetC, setPrimaryColorSetC] = useState([]);
+  const [secondaryColorSetA, setSecondaryColorSetA] = useState([]);
+  const [secondaryColorSetB, setSecondaryColorSetB] = useState([]);
+  const [secondaryColorSetC, setSecondaryColorSetC] = useState([]);
+  const [tertiaryColorSetA, setTertiaryColorSetA] = useState([]);
+  const [tertiaryColorSetB, setTertiaryColorSetB] = useState([]);
+  const [tertiaryColorSetC, setTertiaryColorSetC] = useState([]);
+  const [tertiaryColorSetD, setTertiaryColorSetD] = useState([]);
+  const [tertiaryColorSetE, setTertiaryColorSetE] = useState([]);
+  const [tertiaryColorSetF, setTertiaryColorSetF] = useState([]);
+
+  useEffect(() => {
+    const [
+      newPrimaryColorSetA,
+      newPrimaryColorSetB,
+      newPrimaryColorSetC,
+      newSecondaryColorSetA,
+      newSecondaryColorSetB,
+      newSecondaryColorSetC,
+      newTertiaryColorSetA,
+      newTertiaryColorSetB,
+      newTertiaryColorSetC,
+      newTertiaryColorSetD,
+      newTertiaryColorSetE,
+      newTertiaryColorSetF,
+    ] = expandColor('123456');
+    setPrimaryColorSetA(newPrimaryColorSetA);
+    setPrimaryColorSetB(newPrimaryColorSetB);
+    setPrimaryColorSetC(newPrimaryColorSetC);
+    setSecondaryColorSetA(newSecondaryColorSetA);
+    setSecondaryColorSetB(newSecondaryColorSetB);
+    setSecondaryColorSetC(newSecondaryColorSetC);
+    setTertiaryColorSetA(newTertiaryColorSetA);
+    setTertiaryColorSetB(newTertiaryColorSetB);
+    setTertiaryColorSetC(newTertiaryColorSetC);
+    setTertiaryColorSetD(newTertiaryColorSetD);
+    setTertiaryColorSetE(newTertiaryColorSetE);
+    setTertiaryColorSetF(newTertiaryColorSetF);
+  }, []);
+
+  const handleChange = ({ target: { value } }) => inputValue[0] === '#'
+    ? setInputValue(value.substring(1))
+    : setInputValue(value);
   const handleClick = () => {
-    /* eslint-disable no-unused-vars */
-    const [_, ...hexCode] = inputValue;
-    const colorHex = inputValue[0] === '#'
-      ? hexCode
+    const colorHex = inputValue.length === 3
+      ? `${inputValue[0]}${inputValue[0]}${inputValue[1]}${inputValue[1]}${inputValue[2]}${inputValue[2]}`
       : inputValue;
-    if (colorHex.length === 3) {
-      const hexValue = `${colorHex[0]}${colorHex[0]}${colorHex[1]}${colorHex[1]}${colorHex[2]}${colorHex[2]}`;
-      setColorSet(expandColor(hexValue));
-      setColorHex(hexValue);
-    } else if (colorHex.length === 6) {
-      setColorSet(expandColor(colorHex));
-      setColorHex(colorHex);
-    }
+    const [
+      newPrimaryColorSetA,
+      newPrimaryColorSetB,
+      newPrimaryColorSetC,
+      newSecondaryColorSetA,
+      newSecondaryColorSetB,
+      newSecondaryColorSetC,
+      newTertiaryColorSetA,
+      newTertiaryColorSetB,
+      newTertiaryColorSetC,
+      newTertiaryColorSetD,
+      newTertiaryColorSetE,
+      newTertiaryColorSetF,
+    ] = expandColor(colorHex);
+    setPrimaryColorSetA(newPrimaryColorSetA);
+    setPrimaryColorSetB(newPrimaryColorSetB);
+    setPrimaryColorSetC(newPrimaryColorSetC);
+    setSecondaryColorSetA(newSecondaryColorSetA);
+    setSecondaryColorSetB(newSecondaryColorSetB);
+    setSecondaryColorSetC(newSecondaryColorSetC);
+    setTertiaryColorSetA(newTertiaryColorSetA);
+    setTertiaryColorSetB(newTertiaryColorSetB);
+    setTertiaryColorSetC(newTertiaryColorSetC);
+    setTertiaryColorSetD(newTertiaryColorSetD);
+    setTertiaryColorSetE(newTertiaryColorSetE);
+    setTertiaryColorSetF(newTertiaryColorSetF);
     return;
   };
 
+  const handleKeyDown = ({ key }) => key === 'Enter' && handleClick();
+
   return (
-    <div>
+    <StyledApp>
       <GlobalStyles />
       Color Expander
-      <Input onChange={handleChange} value={inputValue} />
+      <Input onChange={handleChange} value={inputValue} onKeyDown={handleKeyDown} />
       <button onClick={handleClick}>Go</button>
-      {!!colorSet.length && (
+      Primary
+      {!!primaryColorSetA.length && (
         <Results>
-          {colorSet.map((color) => <Result key={color} color={color} active={color === colorHex} />)}
+          {primaryColorSetA.map((color) => <Result key={color} color={color} />)}
         </Results>
       )}
-    </div>
+      {!!primaryColorSetB.length && (
+        <Results>
+          {primaryColorSetB.map((color) => <Result key={color} color={color} />)}
+        </Results>
+      )}
+      {!!primaryColorSetC.length && (
+        <Results>
+          {primaryColorSetC.map((color) => <Result key={color} color={color} />)}
+        </Results>
+      )}
+      Secondary
+      {!!secondaryColorSetA.length && (
+        <Results>
+          {secondaryColorSetA.map((color) => <Result key={color} color={color} />)}
+        </Results>
+      )}
+      {!!secondaryColorSetB.length && (
+        <Results>
+          {secondaryColorSetB.map((color) => <Result key={color} color={color} />)}
+        </Results>
+      )}
+      {!!secondaryColorSetC.length && (
+        <Results>
+          {secondaryColorSetC.map((color) => <Result key={color} color={color} />)}
+        </Results>
+      )}
+      Tertiary
+      {!!tertiaryColorSetA.length && (
+        <Results>
+          {tertiaryColorSetA.map((color) => <Result key={color} color={color} />)}
+        </Results>
+      )}
+      {!!tertiaryColorSetB.length && (
+        <Results>
+          {tertiaryColorSetB.map((color) => <Result key={color} color={color} />)}
+        </Results>
+      )}
+      {!!tertiaryColorSetC.length && (
+        <Results>
+          {tertiaryColorSetC.map((color) => <Result key={color} color={color} />)}
+        </Results>
+      )}
+      {!!tertiaryColorSetD.length && (
+        <Results>
+          {tertiaryColorSetD.map((color) => <Result key={color} color={color} />)}
+        </Results>
+      )}
+      {!!tertiaryColorSetE.length && (
+        <Results>
+          {tertiaryColorSetE.map((color) => <Result key={color} color={color} />)}
+        </Results>
+      )}
+      {!!tertiaryColorSetF.length && (
+        <Results>
+          {tertiaryColorSetF.map((color) => <Result key={color} color={color} />)}
+        </Results>
+      )}
+    </StyledApp>
   );
 };
--- src/utilities/index.js
+++ src/utilities/index.js
@@ -13,15 +13,6 @@ export function formatHex (hex) {
   }
 }
 
-export function expandColor(color) {
-  if (color === 'ffffff') {
-    return [...darkenColor(color), color];
-  } else if (color === '000000') {
-    return [color, ...lightenColor(color)];
-  }
-  return [...darkenColor(color), color, ...lightenColor(color)];
-}
-
 function splitChannels(color) {
   const redChannel = `${color[0]}${color[1]}`;
   const greenChannel = `${color[2]}${color[3]}`;
@@ -29,14 +20,42 @@ function splitChannels(color) {
   return [redChannel, greenChannel, blueChannel]
 }
 
+function rotateHue(color) {
+  const [redChannel, greenChannel, blueChannel] = splitChannels(color);
+  const primaryColorA = `${redChannel}${greenChannel}${blueChannel}`;
+  const primaryColorB = `${blueChannel}${redChannel}${greenChannel}`;
+  const primaryColorC = `${greenChannel}${blueChannel}${redChannel}`;
+  return [primaryColorA, primaryColorB, primaryColorC];
+}
+
+function darkenColor(color) {
+  let nextColor = color;
+  const darkenedColors = [];
+  while (nextColor !== '000000') {
+    const [redChannel, greenChannel, blueChannel] = splitChannels(nextColor);
+    const nextRedChannel = ((parseInt(redChannel, 16) - 17 < 0) ? 0 : parseInt(redChannel, 16) - 17).toString(16);
+    const nextGreenChannel = ((parseInt(greenChannel, 16) - 17 < 0) ? 0 : parseInt(greenChannel, 16) - 17).toString(16);
+    const nextBlueChannel = ((parseInt(blueChannel, 16) - 17 < 0) ? 0 : parseInt(blueChannel, 16) - 17).toString(16);
+    nextColor = `${nextRedChannel.length === 1
+      ? `0${nextRedChannel}`
+      : nextRedChannel}${nextGreenChannel.length === 1
+        ? `0${nextGreenChannel}`
+        : nextGreenChannel}${nextBlueChannel.length === 1
+          ? `0${nextBlueChannel}`
+          : nextBlueChannel}`;
+    darkenedColors.push(nextColor);
+  }
+  return darkenedColors.reverse();
+}
+
 function lightenColor(color) {
   let nextColor = color;
   const lightenedColors = [];
   while (nextColor !== 'ffffff') {
     const [redChannel, greenChannel, blueChannel] = splitChannels(nextColor);
-    const nextRedChannel = ((parseInt(redChannel, 16) + 17 > 255) ? 255 : parseInt(redChannel, 16) + 17).toString(16);
-    const nextGreenChannel = ((parseInt(greenChannel, 16) + 17 > 255) ? 255 : parseInt(greenChannel, 16) + 17).toString(16);
-    const nextBlueChannel = ((parseInt(blueChannel, 16) + 17 > 255) ? 255 : parseInt(blueChannel, 16) + 17).toString(16);
+    const nextRedChannel = ((parseInt(redChannel, 16) + 17 > 255) ? parseInt(255).toString(16) : parseInt(redChannel, 16) + 17).toString(16);
+    const nextGreenChannel = ((parseInt(greenChannel, 16) + 17 > 255) ? parseInt(255).toString(16) : parseInt(greenChannel, 16) + 17).toString(16);
+    const nextBlueChannel = ((parseInt(blueChannel, 16) + 17 > 255) ? parseInt(255).toString(16) : parseInt(blueChannel, 16) + 17).toString(16);
     nextColor = `${nextRedChannel.length === 1
       ? `0${nextRedChannel}`
       : nextRedChannel}${nextGreenChannel.length === 1
@@ -49,22 +68,101 @@ function lightenColor(color) {
   return lightenedColors;
 }
 
-function darkenColor(color) {
-  let nextColor = color;
-  const darkenedColors = [];
-  while (nextColor !== '000000') {
-    const [redChannel, greenChannel, blueChannel] = splitChannels(nextColor);
-    const nextRedChannel = ((parseInt(redChannel, 16) - 17 <= 0) ? 0 : parseInt(redChannel, 16) - 17).toString(16);
-    const nextGreenChannel = ((parseInt(greenChannel, 16) - 17 <= 0) ? 0 : parseInt(greenChannel, 16) - 17).toString(16);
-    const nextBlueChannel = ((parseInt(blueChannel, 16) - 17 <= 0) ? 0 : parseInt(blueChannel, 16) - 17).toString(16);
-    nextColor = `${nextRedChannel.length === 1
-      ? `0${nextRedChannel}`
-      : nextRedChannel}${nextGreenChannel.length === 1
-        ? `0${nextGreenChannel}`
-        : nextGreenChannel}${nextBlueChannel.length === 1
-          ? `0${nextBlueChannel}`
-          : nextBlueChannel}`;
-    darkenedColors.push(nextColor);
+function expandLightness(color) {
+  if (color === 'ffffff') {
+    return [...darkenColor(color), color];
+  } else if (color === '000000') {
+    return [color, ...lightenColor(color)];
   }
-  return darkenedColors.reverse();
+  return [...darkenColor(color), color, ...lightenColor(color)];
+}
+
+function combineColors(colorA, colorB) {
+  const [redChannelA, greenChannelA, blueChannelA] = splitChannels(colorA);
+  const [redChannelB, greenChannelB, blueChannelB] = splitChannels(colorB);
+  let redChannel = (parseInt(redChannelA, 16) + parseInt(redChannelB, 16) > 255) ? parseInt(255).toString(16) : (parseInt(redChannelA, 16) + parseInt(redChannelB, 16)).toString(16);
+  let greenChannel = (parseInt(greenChannelA, 16) + parseInt(greenChannelB, 16) > 255) ? parseInt(255).toString(16) : (parseInt(greenChannelA, 16) + parseInt(greenChannelB, 16)).toString(16);
+  let blueChannel = (parseInt(blueChannelA, 16) + parseInt(blueChannelB, 16) > 255) ? parseInt(255).toString(16) : (parseInt(blueChannelA, 16) + parseInt(blueChannelB, 16)).toString(16);
+  if (redChannel === '0') redChannel = '00';
+  if (greenChannel === '0') greenChannel = '00';
+  if (blueChannel === '0') blueChannel = '00';
+  console.log('COLOR',  `${redChannel}${greenChannel}${blueChannel}`);
+  return `${redChannel}${greenChannel}${blueChannel}`;
+}
+
+function generateSecondaryColors(primaryColorA, primaryColorB, primaryColorC) {
+  const secondaryColorA = combineColors(primaryColorA, primaryColorB);
+  const secondaryColorB = combineColors(primaryColorB, primaryColorC);
+  const secondaryColorC = combineColors(primaryColorC, primaryColorA);
+  return [secondaryColorA, secondaryColorB, secondaryColorC];
+}
+
+function generateTertiaryColors(
+  primaryColorA,
+  primaryColorB,
+  primaryColorC,
+  secondaryColorA,
+  secondaryColorB,
+  secondaryColorC,
+) {
+  const tertiaryColorA = combineColors(primaryColorA, secondaryColorA);
+  const tertiaryColorB = combineColors(secondaryColorA, primaryColorB);
+  const tertiaryColorC = combineColors(primaryColorB, secondaryColorB);
+  const tertiaryColorD = combineColors(secondaryColorB, primaryColorC);
+  const tertiaryColorE = combineColors(primaryColorC, secondaryColorC);
+  const tertiaryColorF = combineColors(secondaryColorC, primaryColorA);
+  return [
+    tertiaryColorA,
+    tertiaryColorB,
+    tertiaryColorC,
+    tertiaryColorD,
+    tertiaryColorE,
+    tertiaryColorF,
+  ];
+}
+
+export function expandColor(color) {
+  const [primaryColorA, primaryColorB, primaryColorC] = rotateHue(color);
+  const primaryColorSetA = expandLightness(primaryColorA);
+  const primaryColorSetB = expandLightness(primaryColorB);
+  const primaryColorSetC = expandLightness(primaryColorC);
+  const [secondaryColorA, secondaryColorB, secondaryColorC] = generateSecondaryColors(primaryColorA, primaryColorB, primaryColorC);
+  const secondaryColorSetA = expandLightness(secondaryColorA);
+  const secondaryColorSetB = expandLightness(secondaryColorB);
+  const secondaryColorSetC = expandLightness(secondaryColorC);
+  const [
+    tertiaryColorA,
+    tertiaryColorB,
+    tertiaryColorC,
+    tertiaryColorD,
+    tertiaryColorE,
+    tertiaryColorF,
+  ] = generateTertiaryColors(
+    primaryColorA,
+    primaryColorB,
+    primaryColorC,
+    secondaryColorA,
+    secondaryColorB,
+    secondaryColorC,
+  );
+  const tertiaryColorSetA = expandLightness(tertiaryColorA);
+  const tertiaryColorSetB = expandLightness(tertiaryColorB);
+  const tertiaryColorSetC = expandLightness(tertiaryColorC);
+  const tertiaryColorSetD = expandLightness(tertiaryColorD);
+  const tertiaryColorSetE = expandLightness(tertiaryColorE);
+  const tertiaryColorSetF = expandLightness(tertiaryColorF);
+  return [
+    primaryColorSetA,
+    primaryColorSetB,
+    primaryColorSetC,
+    secondaryColorSetA,
+    secondaryColorSetB,
+    secondaryColorSetC,
+    tertiaryColorSetA,
+    tertiaryColorSetB,
+    tertiaryColorSetC,
+    tertiaryColorSetD,
+    tertiaryColorSetE,
+    tertiaryColorSetF,
+  ];
 }
