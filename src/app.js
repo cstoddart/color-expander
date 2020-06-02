3f24c75b52b4d8b2f0a8086d67335d30fbcd89f9
Chris Stoddart <cstoddart@rocketmail.com>
2020-06-01 22:08:02 -0500

    fix color mixing, copy to clipboard

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, Fragment } from 'react';
 import styled from 'styled-components';
 
 import { expandColor, formatHex, validateBlackContrast } from './utilities';
@@ -8,51 +8,136 @@ const StyledApp = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: black;
-  padding: 25px;
-`;
+  padding: 50px;
+  position: relative;
 
-const Input = styled.input``;
-
-const ResultsGroup = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-
-  & + & {
-    margin-top: 25px;
+  @media (max-width: 1000px) {
+    paddin5: 25px;
   }
 `;
 
-const ResultsGroupTitle = styled.div`
-  color: white;
+const Logo = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 50px;
 `;
 
-const Results = styled.div`
+const BackgroundColorToggle = styled.div`
+  position: absolute;
+  top: 50px;
+  right: 50px;
+  cursor: pointer;
+`;
+
+const ColorInput = styled.input`
+  padding: 15px;
+  border: none;
+  margin-bottom: 25px;
+  background-color: white;
+  color: black;
+  border: 2px solid black;
+`;
+
+const BlendTypes = styled.div`
+  display: flex;
+`;
+
+const BlendType = styled.div`
+  padding: 5px 10px;
+  font-size: 14px;
+  cursor: pointer;
+  background-color: ${({ active }) => active
+    ? 'var(--text-color)'
+    : 'transparent'
+  };
+  color: ${({ active }) => active
+    ? 'var(--background-color)'
+    : 'var(--text-color)'
+  };
+`;
+
+const ColorSets = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const ColorSetTitle = styled.div`
+  text-transform: capitalize;
+  margin-top: 50px;
+  margin-bottom: 25px;
+  font-size: 18px;
+`;
+
+const ColorSet = styled.div`
   display: flex;
   align-items: center;
 `;
 
-const ResultTooltip = styled.div`
+const CopyToClipboardIcon = styled.div.attrs(({ backgroundColor }) => ({
+  style: {
+    color: `#${backgroundColor}`,
+  },
+}))`
+  position: relative;
+  opacity: 0;
+  transition: opacity 0.2s linear 0.1s;
+  visibility: hidden;
+
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    display: block;
+    height: 8px;
+    width: 6px;
+    border: 1px solid ${({ color }) => color};
+    border-radius: 2px;
+  }
+
+  &:before {
+    top: 0px;
+    right: 0px;
+  }
+  
+  &:after {
+    top: 3px;
+    right: 3px;
+    background-color: currentColor;
+    outline: 0.5px solid currentColor;
+  }
+`;
+
+const Tooltip = styled.div`
   letter-spacing: 0.5px;
   z-index: 1;
   color: ${({ color }) => color};
   opacity: 0;
   visibility: hidden;
-  padding: 10px;
   transition: opacity 0.2s linear 0.1s;
+  align-self: flex-start;
 `;
-  
-const StyledResult = styled.div.attrs(({ color }) => ({
-  style: { background: `#${color}` },
+
+// box-shadow added to address glitchy lines between elements after flex resizing
+const Color = styled.div.attrs(({ color, isLast }) => ({
+  style: {
+    background: `#${color}`,
+    boxShadow: isLast || `1px 0 0 0 #${color}`,
+  },
 }))`
   flex: 1;
   height: 100px;
   min-width: 0;
   position: relative;
-  transition: 0.3s;
+  transition: min-width 0.3s;
   display: flex;
-  align-items: flex-end;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 10px;
+  cursor: pointer;
+  border: ${({ contrastColor, isSelected }) => isSelected && `
+    2px solid ${contrastColor}
+  `};
 
   &:hover {
     min-width: 100px;
@@ -61,20 +146,12 @@ const StyledResult = styled.div.attrs(({ color }) => ({
     `};
   }
 
-  &:hover ${ResultTooltip} {
+  &:hover ${CopyToClipboardIcon},
+  &:hover ${Tooltip} {
     opacity: 1;
     visibility: visible;
   }
 `;
-
-const Result = ({ color }) => {
-  const contrastColor = validateBlackContrast(color) ? 'black' : 'white';
-  return (
-    <StyledResult color={color} contrastColor={contrastColor}>
-      <ResultTooltip color={contrastColor}>#{formatHex(color)}</ResultTooltip>
-    </StyledResult>
-  );
-};
 
 function validateInput(input) {
   if (input.length !== 3 && input.length !== 6) return false;
@@ -82,6 +159,33 @@ function validateInput(input) {
     if (isNaN(parseInt(input[i], 16))) return false;
   }
   return true;
+}
+
+function generateFavicon(color) {
+  const headElement = document.getElementsByTagName('head')[0];
+  const oldFavicon = document.querySelector('link[rel="icon"]');
+  if (oldFavicon) oldFavicon.remove();
+  const faviconLink = document.createElement("link");
+  faviconLink.rel = "icon";
+  const faviconImage = document.createElement('canvas');
+  faviconImage.width = 200;
+  faviconImage.height = 200;
+  const canvasContext = faviconImage.getContext('2d');
+  canvasContext.beginPath();
+  canvasContext.rect(0, 0, 200, 200);
+  canvasContext.fillStyle = `#${color}`;
+  canvasContext.fill();
+  faviconLink.href = faviconImage.toDataURL();
+  headElement.appendChild(faviconLink);
+}
+
+function copyColorToClipboard(color) {
+  const newInput = document.createElement('input');
+  newInput.value = `#${color}`;
+  document.body.appendChild(newInput);
+  newInput.select();
+  document.execCommand('copy');
+  newInput.remove();
 }
 
 export const App = () => {
@@ -98,41 +202,11 @@ export const App = () => {
   const [tertiaryColorSetD, setTertiaryColorSetD] = useState([]);
   const [tertiaryColorSetE, setTertiaryColorSetE] = useState([]);
   const [tertiaryColorSetF, setTertiaryColorSetF] = useState([]);
+  const [selectedColors, setSelectedColors] = useState([{ color: '123456', set: 'primaryA' }]);
+  const [backgroundColor, setBackgroundColor] = useState('dark'); // dark \ light
+  const [blendType, setBlendType] = useState('additive'); // additive | subtractive | average
 
-  useEffect(() => {
-    const [
-      newPrimaryColorSetA,
-      newPrimaryColorSetB,
-      newPrimaryColorSetC,
-      newSecondaryColorSetA,
-      newSecondaryColorSetB,
-      newSecondaryColorSetC,
-      newTertiaryColorSetA,
-      newTertiaryColorSetB,
-      newTertiaryColorSetC,
-      newTertiaryColorSetD,
-      newTertiaryColorSetE,
-      newTertiaryColorSetF,
-    ] = expandColor('123456');
-    setPrimaryColorSetA(newPrimaryColorSetA);
-    setPrimaryColorSetB(newPrimaryColorSetB);
-    setPrimaryColorSetC(newPrimaryColorSetC);
-    setSecondaryColorSetA(newSecondaryColorSetA);
-    setSecondaryColorSetB(newSecondaryColorSetB);
-    setSecondaryColorSetC(newSecondaryColorSetC);
-    setTertiaryColorSetA(newTertiaryColorSetA);
-    setTertiaryColorSetB(newTertiaryColorSetB);
-    setTertiaryColorSetC(newTertiaryColorSetC);
-    setTertiaryColorSetD(newTertiaryColorSetD);
-    setTertiaryColorSetE(newTertiaryColorSetE);
-    setTertiaryColorSetF(newTertiaryColorSetF);
-  }, []);
-
-  const handleChange = ({ target: { value } }) => inputValue[0] === '#'
-    ? setInputValue(value.substring(1))
-    : setInputValue(value);
-
-  const handleClick = () => {
+  const updateColorSets = useCallback(() => {
     if (!validateInput(inputValue)) return;
     const colorHex = inputValue.length === 3
       ? `${inputValue[0]}${inputValue[0]}${inputValue[1]}${inputValue[1]}${inputValue[2]}${inputValue[2]}`
@@ -150,7 +224,7 @@ export const App = () => {
       newTertiaryColorSetD,
       newTertiaryColorSetE,
       newTertiaryColorSetF,
-    ] = expandColor(colorHex);
+    ] = expandColor(colorHex, blendType);
     setPrimaryColorSetA(newPrimaryColorSetA);
     setPrimaryColorSetB(newPrimaryColorSetB);
     setPrimaryColorSetC(newPrimaryColorSetC);
@@ -163,86 +237,99 @@ export const App = () => {
     setTertiaryColorSetD(newTertiaryColorSetD);
     setTertiaryColorSetE(newTertiaryColorSetE);
     setTertiaryColorSetF(newTertiaryColorSetF);
-    return;
-  };
+    setSelectedColors([{ color: colorHex, set: 'primaryA' }]);
+    generateFavicon(colorHex);
+  }, [blendType, inputValue])
 
-  const handleKeyDown = ({ key }) => key === 'Enter' && handleClick();
+  useEffect(() => {
+    updateColorSets()
+  }, [updateColorSets]);
+
+  useEffect(() => {
+    document.body.style.setProperty(
+      '--background-color',
+      backgroundColor === 'dark' ? 'black' : 'white',
+    );
+    document.body.style.setProperty(
+      '--text-color',
+      backgroundColor === 'dark' ? 'white' : 'black',
+    );
+  }, [backgroundColor]);
+
+  const handleColorChange = ({ target: { value } }) => inputValue[0] === '#'
+    ? setInputValue(value.substring(1))
+    : setInputValue(value);
+
+  const colorIsSelected = (color, set) => selectedColors.find((selectedColor) => (
+    selectedColor.color === color && selectedColor.set === set
+  ));
+
+  const toggleBackgroundColor = () => setBackgroundColor(backgroundColor === 'dark' ? 'light' : 'dark');
+
+  const blendTypes = [
+    { id: 'additive', setFunction: () => setBlendType('additive'), label: 'Add'},
+    { id: 'subtractive', setFunction: () => setBlendType('subtractive'), label: 'Subtract'},
+    { id: 'average', setFunction: () => setBlendType('average'), label: 'Average'},
+  ];
+
+  const colorSets = [
+    { id: 'primaryA', set: primaryColorSetA, group: 'primary' },
+    { id: 'primaryB', set: primaryColorSetB, group: 'primary' },
+    { id: 'primaryC', set: primaryColorSetC, group: 'primary' },
+    { id: 'secondaryA', set: secondaryColorSetA, group: 'secondary' },
+    { id: 'secondaryB', set: secondaryColorSetB, group: 'secondary' },
+    { id: 'secondaryC', set: secondaryColorSetC, group: 'secondary' },
+    { id: 'tertiaryA', set: tertiaryColorSetA, group: 'tertiary' },
+    { id: 'tertiaryB', set: tertiaryColorSetB, group: 'tertiary' },
+    { id: 'tertiaryC', set: tertiaryColorSetC, group: 'tertiary' },
+    { id: 'tertiaryD', set: tertiaryColorSetD, group: 'tertiary' },
+    { id: 'tertiaryE', set: tertiaryColorSetE, group: 'tertiary' },
+    { id: 'tertiaryF', set: tertiaryColorSetF, group: 'tertiary' },
+  ];
 
   return (
     <StyledApp>
       <GlobalStyles />
-      Color Expander
-      <Input onChange={handleChange} value={inputValue} onKeyDown={handleKeyDown} />
-      <button onClick={handleClick}>Go</button>
-      <ResultsGroup>
-        <ResultsGroupTitle>Primary</ResultsGroupTitle>
-        {!!primaryColorSetA.length && (
-          <Results>
-            {primaryColorSetA.map((color) => <Result key={color} color={color} />)}
-          </Results>
-        )}
-        {!!primaryColorSetB.length && (
-          <Results>
-            {primaryColorSetB.map((color) => <Result key={color} color={color} />)}
-          </Results>
-        )}
-        {!!primaryColorSetC.length && (
-          <Results>
-            {primaryColorSetC.map((color) => <Result key={color} color={color} />)}
-          </Results>
-        )}
-      </ResultsGroup>
-      <ResultsGroup>
-        <ResultsGroupTitle>Secondary</ResultsGroupTitle>
-        {!!secondaryColorSetA.length && (
-          <Results>
-            {secondaryColorSetA.map((color) => <Result key={color} color={color} />)}
-          </Results>
-        )}
-        {!!secondaryColorSetB.length && (
-          <Results>
-            {secondaryColorSetB.map((color) => <Result key={color} color={color} />)}
-          </Results>
-        )}
-        {!!secondaryColorSetC.length && (
-          <Results>
-            {secondaryColorSetC.map((color) => <Result key={color} color={color} />)}
-          </Results>
-        )}
-      </ResultsGroup>
-      <ResultsGroup>
-        <ResultsGroupTitle>Tertiary</ResultsGroupTitle>
-        {!!tertiaryColorSetA.length && (
-          <Results>
-            {tertiaryColorSetA.map((color) => <Result key={color} color={color} />)}
-          </Results>
-        )}
-        {!!tertiaryColorSetB.length && (
-          <Results>
-            {tertiaryColorSetB.map((color) => <Result key={color} color={color} />)}
-          </Results>
-        )}
-        {!!tertiaryColorSetC.length && (
-          <Results>
-            {tertiaryColorSetC.map((color) => <Result key={color} color={color} />)}
-          </Results>
-        )}
-        {!!tertiaryColorSetD.length && (
-          <Results>
-            {tertiaryColorSetD.map((color) => <Result key={color} color={color} />)}
-          </Results>
-        )}
-        {!!tertiaryColorSetE.length && (
-          <Results>
-            {tertiaryColorSetE.map((color) => <Result key={color} color={color} />)}
-          </Results>
-        )}
-        {!!tertiaryColorSetF.length && (
-          <Results>
-            {tertiaryColorSetF.map((color) => <Result key={color} color={color} />)}
-          </Results>
-        )}
-      </ResultsGroup>
+      <BackgroundColorToggle onClick={toggleBackgroundColor}>
+        {backgroundColor === 'dark' ? 'Light' : 'Dark'}
+      </BackgroundColorToggle>
+      <Logo>Color Expander</Logo>
+      <ColorInput onChange={handleColorChange} value={inputValue} />
+      <BlendTypes>
+        {blendTypes.map(({ id, setFunction, label }) => (
+          <BlendType
+            key={id}
+            onClick={setFunction}
+            active={blendType === id}
+          >
+            {label}
+          </BlendType>
+        ))}
+      </BlendTypes>
+      <ColorSets>
+        {colorSets.map((colorSet, colorSetIndex) => (
+          <Fragment key={colorSet.id}>
+            {colorSet.group !== colorSets[colorSetIndex - 1]?.group && (
+              <ColorSetTitle>{colorSet.group}</ColorSetTitle>
+            )}
+            <ColorSet>
+              {colorSet.set.map((color, colorIndex) => (
+                <Color
+                  key={color}
+                  color={color}
+                  contrastColor={validateBlackContrast(color) ? 'black' : 'white'}
+                  isSelected={colorIsSelected(color, colorSet.id)}
+                  isLast={colorIndex === colorSet.length - 1}
+                  onClick={() => copyColorToClipboard(color)}
+                >
+                  <CopyToClipboardIcon color={validateBlackContrast(color) ? 'black' : 'white'} backgroundColor={color} />
+                  <Tooltip color={validateBlackContrast(color) ? 'black' : 'white'}>#{formatHex(color)}</Tooltip>
+                </Color>
+              ))}
+            </ColorSet>
+          </Fragment>
+        ))}
+      </ColorSets>
     </StyledApp>
   );
 };
--- src/utilities/index.js
+++ src/utilities/index.js
@@ -33,9 +33,15 @@ function darkenColor(color) {
   const darkenedColors = [];
   while (nextColor !== '000000') {
     const [redChannel, greenChannel, blueChannel] = splitChannels(nextColor);
-    let nextRedChannel = ((parseInt(redChannel, 16) - 17 < 0) ? 0 : parseInt(redChannel, 16) - 17).toString(16);
-    let nextGreenChannel = ((parseInt(greenChannel, 16) - 17 < 0) ? 0 : parseInt(greenChannel, 16) - 17).toString(16);
-    let nextBlueChannel = ((parseInt(blueChannel, 16) - 17 < 0) ? 0 : parseInt(blueChannel, 16) - 17).toString(16);
+    let nextRedChannel = ((parseInt(redChannel, 16) - 17 < 0)
+      ? parseInt(0).toString()
+      : parseInt(redChannel, 16) - 17).toString(16);
+    let nextGreenChannel = ((parseInt(greenChannel, 16) - 17 < 0)
+      ? parseInt(0).toString()
+      : parseInt(greenChannel, 16) - 17).toString(16);
+    let nextBlueChannel = ((parseInt(blueChannel, 16) - 17 < 0)
+      ? parseInt(0).toString()
+      : parseInt(blueChannel, 16) - 17).toString(16);
     if (nextRedChannel.length === 1) nextRedChannel = `0${nextRedChannel}`;
     if (nextGreenChannel.length === 1) nextGreenChannel = `0${nextGreenChannel}`;
     if (nextBlueChannel.length === 1) nextBlueChannel = `0${nextBlueChannel}`;
@@ -50,9 +56,15 @@ function lightenColor(color) {
   const lightenedColors = [];
   while (nextColor !== 'ffffff') {
     const [redChannel, greenChannel, blueChannel] = splitChannels(nextColor);
-    let nextRedChannel = ((parseInt(redChannel, 16) + 17 > 255) ? parseInt(255).toString(16) : parseInt(redChannel, 16) + 17).toString(16);
-    let nextGreenChannel = ((parseInt(greenChannel, 16) + 17 > 255) ? parseInt(255).toString(16) : parseInt(greenChannel, 16) + 17).toString(16);
-    let nextBlueChannel = ((parseInt(blueChannel, 16) + 17 > 255) ? parseInt(255).toString(16) : parseInt(blueChannel, 16) + 17).toString(16);  
+    let nextRedChannel = ((parseInt(redChannel, 16) + 17 > 255)
+      ? parseInt(255).toString(16)
+      : parseInt(redChannel, 16) + 17).toString(16);
+    let nextGreenChannel = ((parseInt(greenChannel, 16) + 17 > 255)
+      ? parseInt(255).toString(16)
+      : parseInt(greenChannel, 16) + 17).toString(16);
+    let nextBlueChannel = ((parseInt(blueChannel, 16) + 17 > 255)
+      ? parseInt(255).toString(16)
+      : parseInt(blueChannel, 16) + 17).toString(16);  
     if (nextRedChannel.length === 1) nextRedChannel = `0${nextRedChannel}`;
     if (nextGreenChannel.length === 1) nextGreenChannel = `0${nextGreenChannel}`;
     if (nextBlueChannel.length === 1) nextBlueChannel = `0${nextBlueChannel}`;
@@ -71,22 +83,78 @@ function expandLightness(color) {
   return [...darkenColor(color), color, ...lightenColor(color)];
 }
 
-function combineColors(colorA, colorB) {
+function addColors(colorA, colorB) {
   const [redChannelA, greenChannelA, blueChannelA] = splitChannels(colorA);
   const [redChannelB, greenChannelB, blueChannelB] = splitChannels(colorB);
-  let redChannel = (parseInt(redChannelA, 16) + parseInt(redChannelB, 16) > 255) ? parseInt(255).toString(16) : (parseInt(redChannelA, 16) + parseInt(redChannelB, 16)).toString(16);
-  let greenChannel = (parseInt(greenChannelA, 16) + parseInt(greenChannelB, 16) > 255) ? parseInt(255).toString(16) : (parseInt(greenChannelA, 16) + parseInt(greenChannelB, 16)).toString(16);
-  let blueChannel = (parseInt(blueChannelA, 16) + parseInt(blueChannelB, 16) > 255) ? parseInt(255).toString(16) : (parseInt(blueChannelA, 16) + parseInt(blueChannelB, 16)).toString(16);
+  const mixedRed = parseInt(redChannelA, 16) + parseInt(redChannelB, 16);
+  const mixedGreen = parseInt(greenChannelA, 16) + parseInt(greenChannelB, 16);
+  const mixedBlue = parseInt(blueChannelA, 16) + parseInt(blueChannelB, 16);
+  let redChannel = (mixedRed > 255)
+    ? parseInt(255).toString(16)
+    : mixedRed.toString(16);
+  let greenChannel = (mixedGreen > 255)
+    ? parseInt(255).toString(16)
+    : mixedGreen.toString(16);
+  let blueChannel = (mixedBlue > 255)
+    ? parseInt(255).toString(16)
+    : mixedBlue.toString(16);
   if (redChannel.length === 1) redChannel = `0${redChannel}`;
   if (greenChannel.length === 1) greenChannel = `0${greenChannel}`;
   if (blueChannel.length === 1) blueChannel = `0${blueChannel}`;
   return `${redChannel}${greenChannel}${blueChannel}`;
 }
 
-function generateSecondaryColors(primaryColorA, primaryColorB, primaryColorC) {
-  const secondaryColorA = combineColors(primaryColorA, primaryColorB);
-  const secondaryColorB = combineColors(primaryColorB, primaryColorC);
-  const secondaryColorC = combineColors(primaryColorC, primaryColorA);
+function subtractColors(colorA, colorB) {
+  const [redChannelA, greenChannelA, blueChannelA] = splitChannels(colorA);
+  const [redChannelB, greenChannelB, blueChannelB] = splitChannels(colorB);
+  const mixedRed = parseInt(redChannelA, 16) - parseInt(redChannelB, 16);
+  const mixedGreen = parseInt(greenChannelA, 16) - parseInt(greenChannelB, 16);
+  const mixedBlue = parseInt(blueChannelA, 16) - parseInt(blueChannelB, 16);
+  let redChannel = (mixedRed < 0)
+    ? parseInt(0).toString(16)
+    : mixedRed.toString(16);
+  let greenChannel = (mixedGreen < 0)
+    ? parseInt(0).toString(16)
+    : mixedGreen.toString(16);
+  let blueChannel = (mixedBlue < 0)
+    ? parseInt(0).toString(16)
+    : mixedBlue.toString(16);
+  if (redChannel.length === 1) redChannel = `0${redChannel}`;
+  if (greenChannel.length === 1) greenChannel = `0${greenChannel}`;
+  if (blueChannel.length === 1) blueChannel = `0${blueChannel}`;
+  return `${redChannel}${greenChannel}${blueChannel}`;
+}
+
+function averageColors(colorA, colorB) {
+  const [redChannelA, greenChannelA, blueChannelA] = splitChannels(colorA);
+  const [redChannelB, greenChannelB, blueChannelB] = splitChannels(colorB);
+  const mixedRed = Math.round((parseInt(redChannelA, 16) + parseInt(redChannelB, 16)) / 2);
+  const mixedGreen = Math.round((parseInt(greenChannelA, 16) + parseInt(greenChannelB, 16)) / 2);
+  const mixedBlue = Math.round((parseInt(blueChannelA, 16) + parseInt(blueChannelB, 16)) / 2);
+  let redChannel = (mixedRed > 255)
+    ? parseInt(255).toString(16)
+    : mixedRed.toString(16);
+  let greenChannel = (mixedGreen > 255)
+    ? parseInt(255).toString(16)
+    : mixedGreen.toString(16);
+  let blueChannel = (mixedBlue > 255)
+    ? parseInt(255).toString(16)
+    : mixedBlue.toString(16);
+  if (redChannel.length === 1) redChannel = `0${redChannel}`;
+  if (greenChannel.length === 1) greenChannel = `0${greenChannel}`;
+  if (blueChannel.length === 1) blueChannel = `0${blueChannel}`;
+  return `${redChannel}${greenChannel}${blueChannel}`;
+}
+
+function generateSecondaryColors(primaryColorA, primaryColorB, primaryColorC, blendType) {
+  const blendColors = blendType === 'additive'
+    ? addColors
+    : blendType === 'subtractive'
+    ? subtractColors
+    : averageColors;
+  const secondaryColorA = blendColors(primaryColorA, primaryColorB);
+  const secondaryColorB = blendColors(primaryColorB, primaryColorC);
+  const secondaryColorC = blendColors(primaryColorC, primaryColorA);
   return [secondaryColorA, secondaryColorB, secondaryColorC];
 }
 
@@ -97,13 +165,19 @@ function generateTertiaryColors(
   secondaryColorA,
   secondaryColorB,
   secondaryColorC,
+  blendType,
 ) {
-  const tertiaryColorA = combineColors(primaryColorA, secondaryColorA);
-  const tertiaryColorB = combineColors(secondaryColorA, primaryColorB);
-  const tertiaryColorC = combineColors(primaryColorB, secondaryColorB);
-  const tertiaryColorD = combineColors(secondaryColorB, primaryColorC);
-  const tertiaryColorE = combineColors(primaryColorC, secondaryColorC);
-  const tertiaryColorF = combineColors(secondaryColorC, primaryColorA);
+  const blendColors = blendType === 'additive'
+    ? addColors
+    : blendType === 'subtractive'
+    ? subtractColors
+    : averageColors;
+  const tertiaryColorA = blendColors(primaryColorA, secondaryColorA);
+  const tertiaryColorB = blendColors(secondaryColorA, primaryColorB);
+  const tertiaryColorC = blendColors(primaryColorB, secondaryColorB);
+  const tertiaryColorD = blendColors(secondaryColorB, primaryColorC);
+  const tertiaryColorE = blendColors(primaryColorC, secondaryColorC);
+  const tertiaryColorF = blendColors(secondaryColorC, primaryColorA);
   return [
     tertiaryColorA,
     tertiaryColorB,
@@ -114,12 +188,12 @@ function generateTertiaryColors(
   ];
 }
 
-export function expandColor(color) {
+export function expandColor(color, blendType) {
   const [primaryColorA, primaryColorB, primaryColorC] = rotateHue(color);
   const primaryColorSetA = expandLightness(primaryColorA);
   const primaryColorSetB = expandLightness(primaryColorB);
   const primaryColorSetC = expandLightness(primaryColorC);
-  const [secondaryColorA, secondaryColorB, secondaryColorC] = generateSecondaryColors(primaryColorA, primaryColorB, primaryColorC);
+  const [secondaryColorA, secondaryColorB, secondaryColorC] = generateSecondaryColors(primaryColorA, primaryColorB, primaryColorC, blendType);
   const secondaryColorSetA = expandLightness(secondaryColorA);
   const secondaryColorSetB = expandLightness(secondaryColorB);
   const secondaryColorSetC = expandLightness(secondaryColorC);
@@ -137,6 +211,7 @@ export function expandColor(color) {
     secondaryColorA,
     secondaryColorB,
     secondaryColorC,
+    blendType,
   );
   const tertiaryColorSetA = expandLightness(tertiaryColorA);
   const tertiaryColorSetB = expandLightness(tertiaryColorB);
@@ -167,6 +242,5 @@ export function validateBlackContrast(color) {
   const green = parseInt(greenChannel, 16);
   const blue = parseInt(blueChannel, 16);
   const contrast = red * 0.299 + green * 0.587 + blue * 0.114;
-
   return contrast > 125 ? true : false;
 }
